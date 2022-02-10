bf4f6a71a249c3924c1dc3a3619950a345028858
TaiJi2022 <99245302+TaiJi2022@users.noreply.github.com>
2022-02-11 00:56:37 +0800

    Update ppcat.js

--- ppcat.js
+++ ppcat.js
@@ -11,7 +11,7 @@ hostname = api-access.pangolin-sdk-toutiao.com
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj['message'] = "";
+obj['message'] = "ppcat";
 body = JSON.stringify(obj);
 
 console.log(body);
