e9ea70fbd64daf559fc905e496d1eb29c3681b07
TaiJi2022 <99245302+TaiJi2022@users.noreply.github.com>
2022-11-10 11:48:49 +0800

    Update mhybbs.js

--- mhybbs.js
+++ mhybbs.js
@@ -123,6 +123,7 @@ function GetCookie() {
   } 
 
 function captchaPass(gt, challenge) {
+   $.info('自动校验' + gt + ',' + challenge)
   const geetest = 'https://apiv6.geetest.com/ajax.php?gt=' + gt + '&challenge='+ challenge + '&lang=zh-cn&pt=3&client_type=web_mobile&callback=geetest_1665115368313'
   return $.http.get(geetest).then((res) => {
     const jsonp = res.body.text()
